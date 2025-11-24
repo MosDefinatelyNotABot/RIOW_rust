48af6f2ddf0f1b164cd6c2a29b5da3eb321ed730
Joshua Britz <brtjos005@myuct.ac.za>
2025-11-24 14:57:48 +0200

    working. end of chap 8

--- src/camera.rs
+++ src/camera.rs
@@ -6,12 +6,13 @@ use ultraviolet::Vec3;
 use itertools::iproduct;
 use crate::hittable::{Hittable, HittableList};
 use crate::ray::Ray;
-use crate::sphere::Sphere;
 
 pub struct Camera {
     width: u32,
     height: u32,
     aspect_ratio: f32,
+    px_samples: u32,
+    px_samples_scale: f32,
     image: RgbImage,
 
     focal_length: f32,
@@ -25,13 +26,11 @@ pub struct Camera {
     px_delta_u: Vec3,
     px_delta_v: Vec3,
     px_loc_100: Vec3,
-
-    world: HittableList
 }
 
 impl Camera {
 
-    pub fn init(height: u32, aspect: f32, focal: f32) -> Self {
+    pub fn init(height: u32, aspect: f32, samples_per_px: u32, focal: f32) -> Self {
 
         // camera setup
         let width = (height as f32 * aspect) as u32;
@@ -51,19 +50,16 @@ impl Camera {
             - viewport_v / 2.0;
         let px_loc_100 = viewport_up_left + 0.5 * (px_delta_u + px_delta_v);
 
-        // Setup the scene
-        let mut world = HittableList::new();
-
-        world.add(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
-        world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
-
         Camera{
+
             width,
             height,
             aspect_ratio: aspect,
+            px_samples: samples_per_px,
+            px_samples_scale: 1.0 / (samples_per_px as f32),
             image: image::RgbImage::new(width, height),
-            focal_length: focal,
 
+            focal_length: focal,
             viewport_height,
             viewport_width,
 
@@ -75,29 +71,21 @@ impl Camera {
             px_delta_v,
             px_loc_100,
 
-            world,
         }
 
     }
 
-    pub fn render(&mut self) {
+    pub fn render(&mut self, world: &HittableList) {
 
-        let pg_bar = ProgressBar::new(self.height as u64 * self.width as u64);
-        pg_bar.set_style(
-            ProgressStyle::with_template("elapsed: [{elapsed}] eta: [{eta_precise}] {bar:50.cyan/blue} {pos:>7}/{len:7} {msg}")
-            .unwrap()
-            .progress_chars("##-"));
-        pg_bar.set_message("rendering frame...");
+        let pg_bar = self.setup_pg_bar(); // setup progress bar
 
         for (x, y) in iproduct!((0..self.width).into_iter(), (0..self.height).into_iter()) {
 
-            let px_center = self.px_loc_100 + (self.px_delta_u * x as f32) + (self.px_delta_v * y as f32);
-            let ray_dir = px_center - self.origin;
-            let ray: Ray = Ray::new(&self.origin, &ray_dir);
+            let col: Vec3 = (0..self.px_samples)                       // for each pixel sample
+                .map(|_| Camera::ray_colour(&self.get_ray(x, y), world)) // calc the pixel colour
+                .sum::<Vec3>() * self.px_samples_scale;                  // then accumulate and scale.
 
-            let col: Rgb<u8> = ray_colour(&ray, &self.world);
-
-            self.image.put_pixel(x, y, col);
+            self.image.put_pixel(x, y, Rgb([col.x as u8, col.y as u8, col.z as u8]));
             pg_bar.inc(1);
 
         }
@@ -107,45 +95,74 @@ impl Camera {
     }
 
     pub fn save(&self, filename: &str) {
+        // saves the previously rendered image
 
         let mut out_dir = PathBuf::new();
         out_dir.push("output_images");
 
         if !out_dir.exists() {
-            std::fs::create_dir(&out_dir).unwrap();
+            let res = std::fs::create_dir(&out_dir);
+            res.unwrap_or_else(|e| println!("Error creating output directory: {e}"));
         }
 
         out_dir = out_dir.join(filename);
         out_dir.set_extension("png");
 
-        self.image.save(&out_dir).unwrap_or_else(|e| println!("Error saving image {e}"));
+        self.image.save(&out_dir).unwrap_or_else(|e| println!("Error saving image: {e}"));
 
         println!("Saved rendered image to {}", &out_dir.to_str().unwrap());
 
     }
 
-}
+    fn setup_pg_bar(&self) -> ProgressBar {
+        // set up the progress bar...
 
+        let pg_bar = ProgressBar::new(self.height as u64 * self.width as u64);
+        pg_bar.set_style(
+            ProgressStyle::with_template("elapsed: [{elapsed}] eta: [{eta_precise}] {bar:50.cyan/blue} {pos:>7}/{len:7} {msg}")
+                .unwrap()
+                .progress_chars("##-"));
+        pg_bar.set_message("rendering frame...");
 
-pub fn ray_colour(ray: &Ray, world: &HittableList) -> Rgb<u8> {
-
-    let t = world.hit(ray, 0.0, f32::INFINITY) ;
-
-    if t.is_some() {
-        // calculate the surface normal of the sphere if it's hit
-
-        let mut normal: Vec3  = 0.5 * (t.unwrap().normal + Vec3::new(1.0, 1.0, 1.0));
-        normal = 255.0 * normal; // convert to rgb values
-
-        return Rgb([normal.x as u8, normal.y as u8, normal.z as u8]);
+        pg_bar
 
     }
 
-    let unit_dir = ray.direction.normalized();
-    let a = 0.5 * (unit_dir.y + 1.0);
+    fn ray_colour(ray: &Ray, world: &HittableList) -> Vec3 {
 
-    let out = (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.5, 0.7, 1.0);
+        let t = world.hit(ray, 0.0, f32::INFINITY) ;
 
-    Rgb([(out.x * 255.0) as u8, (out.y * 255.0) as u8, (out.z * 255.0) as u8])
+        if t.is_some() {
+            // calculate the surface normal of the sphere if it's hit
+
+            let normal: Vec3  = 0.5 * (t.unwrap().normal + Vec3::new(1.0, 1.0, 1.0));
+            return 255.0 * normal; // convert to rgb values
+
+        }
+
+        let unit_dir = ray.direction.normalized();
+        let a = 0.5 * (unit_dir.y + 1.0);
+
+        let out = (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.5, 0.7, 1.0);
+
+        255.0 * out
+
+    }
+
+    fn get_ray(&self, u: u32, v: u32) -> Ray {
+        // generates a ray originating from the camera center directed at a randomly sampled
+        // point centered at pixel i j
+
+        let offset: (f32, f32) = rand::random();
+
+        let px_sample = self.px_loc_100
+            + ( (u as f32 + offset.0) * self.px_delta_u)
+            + ( (v as f32 + offset.1) * self.px_delta_v);
+
+        Ray::new(&self.origin, &(px_sample - self.origin))
+
+    }
 
 }
+
+
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,33 @@ mod ray;
 mod hittable;
 mod sphere;
 
+use ultraviolet::Vec3;
 use crate::camera::{Camera};
+use crate::hittable::HittableList;
+use crate::sphere::Sphere;
 
 fn main() {
 
-    println!("Hello, world!");
+    println!("Ray Tracing in One Weekend.\n\
+              ===========================\n");
 
+    // camera setup
     let mut frame_obj = Camera::init(
         720,            // height 720p
         1.7777778,     // aspect ratio 16:9
+        64,
         1.0             // focal length 1.0 in camera space
     );
 
-    frame_obj.render();
+    // scene setup
+    let mut world = HittableList::new();
+    world.add(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
+    world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
 
+    // render scene
+    frame_obj.render(&world);
+
+    // save rendered image to file
     frame_obj.save("test.png");
 
 }
